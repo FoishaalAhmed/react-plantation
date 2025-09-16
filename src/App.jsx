0937e2760a87d905a10507a4a42786495b1039e0
Foisal Ahmed <foisal.cse@gmail.com>
2025-09-16 22:30:34 +0600

    feat::item dynamic done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useState } from 'react'
+import React from 'react'
 import NavBar from './Components/Nav/NavBar'
 import Footer from './Components/Nav/Footer'
 import Slider from './Components/Slider'
@@ -7,38 +7,96 @@ import Button from './Components/Button'
 import Item from './Components/Main/Item'
 import { initialState } from './data/plant'
 
+const prices = [...new Set(initialState.map((item) => item.price))].sort((a, b) => b - a).slice(0, 10); 
+
+let activeCategory = "All";
+
+const filterablePrices = {
+	All: [...new Set(initialState.map((item) => item.price))],
+	Indore: [...new Set(initialState.filter((item) => item.type === "Indore").map((item) => item.price))],
+	Calathea: [...new Set(initialState.filter((item) => item.type === "Calathea").map((item) => item.price))],
+	Dracaena: [...new Set(initialState.filter((item) => item.type === "Dracaena").map((item) => item.price))],
+	Ficus: [...new Set(initialState.filter((item) => item.type === "Ficus").map((item) => item.price))],
+	Orchids: [...new Set(initialState.filter((item) => item.type === "Orchids").map((item) => item.price))],
+};
+
+const reducer = (state, action) => {
+
+	switch (action.type) {
+
+		case "All": {
+			return initialState;
+		}
+
+		case "Indore": {
+			activeCategory = "Indore";
+			return initialState.filter((item) => item.type == "Indore");
+		}
+
+		case "Calathea": {
+			activeCategory = "Calathea";
+			return initialState.filter((item) => item.type == "Calathea");
+		}
+
+		case "Dracaena": {
+			activeCategory = "Dracaena";
+			return initialState.filter((item) => item.type == "Dracaena");
+		}
+
+		case "Ficus": {
+			activeCategory = "Ficus";
+			return initialState.filter((item) => item.type == "Ficus");
+		}
+
+		case "Orchids": {
+			activeCategory = "Orchids";
+			return initialState.filter((item) => item.type == "Orchids");
+		}
+	
+		default: {
+			activeCategory = "All";
+			return initialState;
+		}
+
+	}
+};
+
+const ItemContext = React.createContext();
+
 function App() {
 
-  const prices = [...new Set(initialState.map((item) => item.price))].sort((a, b) => b - a).slice(0, 10); 
+	const [items, dispatch] = React.useReducer(reducer, initialState);
 
-  let ActiveCategory = "All";
+	return (
+		<div>
+			<ItemContext.Provider
+						value={{
+							items: items,
+							itemDispatch: dispatch,
+							activeCategory: activeCategory,
+							itemCurrentPage: 1,
+							filterPrice: filterablePrices[activeCategory],
+						}}
+					>
+					<NavBar />
 
-  const filterablePrices = {
-    All: [...new Set(initialState.map((item) => item.price))],
-    Indore: [...new Set(initialState.filter((item) => item.type === "Indore").map((item) => item.price))],
-    Calathea: [...new Set(initialState.filter((item) => item.type === "Calathea").map((item) => item.price))],
-    Dracaena: [...new Set(initialState.filter((item) => item.type === "Dracaena").map((item) => item.price))],
-    Ficus: [...new Set(initialState.filter((item) => item.type === "Ficus").map((item) => item.price))],
-    Orchids: [...new Set(initialState.filter((item) => item.type === "Orchids").map((item) => item.price))],
-  };
+					<Slider></Slider>
 
-  return (
-    <div>
-      <NavBar />
-      <Slider></Slider>
-      <div className="max-w-7xl mx-auto mt-6 flex flex-wrap justify-center gap-2">
-        {prices.map((price) => (
-          <Button key={price} text={price}></Button>
-        ))}
-      </div>
+					<div className="max-w-7xl mx-auto mt-6 flex flex-wrap justify-center gap-2">
+						{prices.map((price) => (
+						<Button key={price} text={price}></Button>
+						))}
+					</div>
 
-      <div className="max-w-7xl mx-auto mt-6">
-        <Item></Item>
-      </div>
+					<div className="max-w-7xl mx-auto mt-6">
+						<Item></Item>
+					</div>
+					</ItemContext.Provider>
 
-      <Footer></Footer>
-    </div>
-  )
+			<Footer></Footer>
+		</div>
+	)
 }
 
+export { ItemContext };
 export default App
--- src/Components/Nav/NavItem.jsx
+++ src/Components/Nav/NavItem.jsx
@@ -1,9 +1,9 @@
-export default function NavItem({navLink, navText, isActive}) {
+export default function NavItem({navText, isActive}) {
     return (
         
         <li>
             <a
-                href={navLink}
+                href="javascript:;"
                 className={`${isActive 
                 ? "text-white bg-blue-700 md:bg-transparent md:text-blue-700 md:dark:text-blue-500" 
                 : "text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"} 
